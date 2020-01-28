44c8ee1f6d9dc51707b0ebe4cadfcb3e41820402
Dargrotek <dargrotek@googlemail.com>
2020-01-28 18:09:13 +0000

    Update Configuration.h
    Updated ABL_GRID_POINTS_X from 5 to 10

--- Configuration.h
+++ Configuration.h
@@ -830,7 +830,7 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define ABL_GRID_POINTS_X 5
+  #define ABL_GRID_POINTS_X 10
   #define ABL_GRID_POINTS_Y ABL_GRID_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
